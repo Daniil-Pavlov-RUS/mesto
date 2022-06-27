8276afee723e27e965a5338f00604a58e16b72c5
Daniil Pavlov <89415322+Daniil-Pavlov-RUS@users.noreply.github.com>
2022-06-27 22:31:23 +0300

    Add files via upload

---
+++ src/script/PopupWithImage.js
@@ -0,0 +1,16 @@
+/* Создайте класс PopupWithImage, который наследует от Popup. Этот класс должен перезаписывать родительский метод open. В методе open класса PopupWithImage нужно вставлять в попап картинку с src изображения и подписью к картинке. */
+
+import Popup from './Popup.js'
+
+export default class PopupWithImage extends Popup {
+    _popupImageElement = this._popupElement.querySelector('.popup__image');
+    _popupImageСaptionElement = this._popupElement.querySelector('.popup__text');
+
+    open(item) {
+        this._popupImageСaptionElement.textContent = item.name;
+        this._popupImageElement.src = item.link;
+        this._popupImageElement.alt = `${item.name}. Фотография`;
+
+        super.open();
+    }
+}
